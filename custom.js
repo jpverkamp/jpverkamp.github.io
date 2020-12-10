75537cbc43cb6becc205af684ea240da4d1df31b
JP Verkamp <me@jverkamp.com>
2020-12-10 17:59:10 -0500

    Automatic deployment (Post on stack machines)

--- custom.js
+++ custom.js
@@ -26,3 +26,33 @@ $( '[data-fancybox="gallery"]' ).fancybox({
         return caption;
     }
 });
+
+/*
+Tabs
+Source: https://www.w3schools.com/howto/howto_js_tabs.asp
+*/
+
+function changeTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+} 
+
+$(function() {
+    document.querySelectorAll('.tablinks.default').forEach((el) => el.click());
+});
