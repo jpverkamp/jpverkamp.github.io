43b465a045f63ab7d4d9d4c53f3fc5d7a7dc63b5
JP Verkamp <github@jverkamp.com>
2022-03-18 12:40:56 -0400

    Automatic deployment (Back to runelang!)

--- embeds/runelang/runelang/parser.js
+++ embeds/runelang/runelang/parser.js
@@ -23,31 +23,33 @@ export default function parse(tokens) {
       log.error("unable to parse expression, got EOF")
     }
 
+    // Parse using the [Shunting-Yard algorithm](https://en.wikipedia.org/wiki/Shunting-yard_algorithm)
     let output = []
     let operator_stack = []
 
+    // Loop through tokens until we hit a token that can't be part of the expression
     while (tokens.length > 0) {
       let token = tokens.shift()
       let text = token.token
 
-      log.debug("Parsing expression", token, { token, output, operator_stack })
-
+      // Literal string
       if (text[0] === '"') {
-        // Literal string
         output.push({ type: "literal", mode: "string", value: text.substring(1, text.length - 1), token })
-      } else if (operator_stack.includes("(") && text === ",") {
-        // A parameter list
-        // TODO: This feels wrong
-      } else if (text in EVAL_CONSTANTS) {
-        // A constant
+      }
+      // A parameter list
+      else if (operator_stack.includes("(") && text === ",") {
+      }
+      // A constant
+      else if (text in EVAL_CONSTANTS) {
         output.push({ type: "literal", mode: "constant", value: EVAL_CONSTANTS[text], token })
-      } else if (text in EVAL_FUNCTIONS) {
-        // A predefined function
+      }
+      // A predefined function
+      else if (text in EVAL_FUNCTIONS) {
         operator_stack.push(text)
-      } else if (text.match(/^\-?[0-9]/)) {
-        // Some kind of number
-
-        // Rational
+      }
+      // Some kind of number
+      else if (text.match(/^\-?[0-9]/)) {
+        // Rational numbers/fractions
         // TODO: Divided by zero error
         if (text.match(/\//)) {
           let [num, den] = text.split("/")
@@ -75,12 +77,14 @@ export default function parse(tokens) {
         else {
           output.push({ type: "literal", mode: "number", value: parseInt(text), token })
         }
-      } else if (text == "(") {
-        // Parens
+      }
+      // Parens, either a function call or grouping
+      else if (text == "(") {
         operator_stack.push("(")
-      } else if (text == ")") {
+      }
+      // Closing parens
+      else if (text == ")") {
         if (operator_stack.includes("(")) {
-          // Closing parens
           while (true) {
             let token = operator_stack.pop()
             if (token === "(") {
@@ -93,7 +97,6 @@ export default function parse(tokens) {
           if (operator_stack.length > 0) {
             let maybeFunction = operator_stack.pop()
             if (maybeFunction in EVAL_FUNCTIONS) {
-              // TODO: Should I be doingEVAL_FUNCTIONS[maybeFunction] this here?
               let f = EVAL_FUNCTIONS[maybeFunction]
               let arity = f
                 .toString()
@@ -110,10 +113,10 @@ export default function parse(tokens) {
           tokens.unshift(token)
           break
         }
-      } else if (text in EVAL_OPERATORS) {
-        // An operator
-        // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-
+      }
+      // An operator
+      // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+      else if (text in EVAL_OPERATORS) {
         let precedence = EVAL_OPERATORS[text].precedence
         log.debug(`Working on operator`, token, { text, precedence })
 
@@ -133,24 +136,30 @@ export default function parse(tokens) {
           output.push({ type: "operator", value: operator_stack.pop() })
         }
         operator_stack.push(text)
-      } else if (text[0].match(/[a-zA-Z]/) && !EVAL_KEYWORDS.includes(text)) {
-        // A variable
+      }
+      // A variable
+      else if (text[0].match(/[a-zA-Z]/) && !EVAL_KEYWORDS.includes(text)) {
         output.push({ type: "variable", value: text, token })
-      } else {
-        // Anything else means that we're done parsing the expression
-        // Remember to put the token back!
+      }
+      // Anything else means that we're done parsing the expression
+      // Remember to put the token back!
+      else {
         tokens.unshift(token)
         break
       }
     }
     log.debug(`Done parsing expression`, token, { output, operator_stack })
 
+    // After we exit the list, any remaining operators get added to the stack
     while (operator_stack.length > 0) {
       output.push({ type: "operator", value: operator_stack.pop() })
     }
 
+    // Construct the result value
     let result = { type: "expression", rpn: output, token }
 
+    // Special case: if the expression was a single identifier
+    // It's probably being used as a variable name in a param/kwarg, store an alias for this
     if (output.length === 1 && output[0].type === "variable") {
       result.asName = output[0].value
     }
@@ -253,92 +262,117 @@ export default function parse(tokens) {
   }
 
   function parseParams() {
+    // Consume the leading (
     let token = tokens.shift()
     log.debug("parseParams", token)
 
     let args = []
     let kwargs = {}
 
-    if (tokens.length === 0) {
-      log.error("unterminated params", token)
-    }
-
+    // Start by parsing args (not kwargs) until we see a kwarg (with a `:`)
     let parsingKwargs = false
     while (true) {
+      // Failure state: no closing )
       if (tokens.length === 0) {
         log.error("unterminated params", token, { expected: ")", got: "EOF" })
-      } else if (tokens[0].token === ")") {
+      }
+      // End state: closing ), consume it and return the parsed params
+      else if (tokens[0].token === ")") {
         tokens.shift()
         return { type: "params", args, kwargs, token }
       }
 
+      // Parse the first part of the expression:
+      // * When in a define, this should always be a single name
+      // * When in a call as an arg, this can be a name or a complicated expression
+      // * When in a kwarg, this is always a name
       let identifier = parseExpression()
 
+      // Failure states: ran out of input or we've started parsing kwargs and got a , (should be a :)
       if (tokens.length === 0) {
         log.error("unterminated params, expected , or : got EOF", token)
       } else if (parsingKwargs && tokens[0].token === ",") {
         log.error("args cannot come after kwargs", token, { expected: ";", got: "," })
       }
 
-      // Parsing a kwarg (key:value)
+      // If the next token is a : switch to kwargs mode and parse the value of the key/value pair
       if (tokens[0].token === ":") {
         tokens.shift()
         parsingKwargs = true
 
+        // This is where we check the above case that for kwargs the left side of the `:` must just be a name
+        // .asName is populated by `parseExpression`, when it returns a single identifier it will be filled
         if (!identifier.asName) {
           log.error("kwargs key must be an identifier", token, { expected: "identifer", got: token })
         }
 
+        // Parsing kwargs and ran out of input
         if (tokens.length === 0) {
           log.error("missing kwargs body", key)
         }
 
+        // Parsing the value
         let value = parseExpression()
 
+        // Cannot have duplicate values in kwargs (we'll check that there are no duplicates in args at runtime)
         if (identifier.asName in kwargs) {
           log.error("duplicate kwarg", token, { name: identifier.asName })
         }
 
+        // All kwargs are also positional `args`, store the new arg in both places here
         args.push(identifier)
         kwargs[identifier.asName] = value
-      } else {
+      }
+      // A regular arg, not a kwarg, just store it
+      else {
         args.push(identifier)
       }
 
+      // If the next argument is a `,` we're still parsing the list
+      // If it's ) we'll catch the end of the list at the top of the loop
+      // If it's anything else, we have malformed input (this previously caused an infinite loop)
       if (tokens[0].token === ",") {
         tokens.shift()
+      } else if (tokens[0].token === ")") {
+        continue
+      } else {
+        log.error("badly formed parameters", token, { expected: [",", ")"], got: tokens[0] })
       }
     }
   }
 
   function parseList() {
+    // Consume the opening [
     let token = tokens.shift()
     log.debug("parseList", token)
 
     let nodes = []
 
     // Lists can be:
-    // literal-list:    [ <body:node>* ]
-    // for-list:        [ <body:node>* for <variable:identifier> in <expression:iterable> ]
-    // times-list:      [ <body:node>* times <variable:integer> ]
+    // literal-list:    [ <nodes:node>* ]
+    // for-list:        [ <nodes:node>* for <variable:identifier> in <expression:iterable> ]
+    // times-list:      [ <nodes:node>* times <variable:integer> ]
 
     while (true) {
+      // Error state: never saw a closing ]
       if (tokens.length === 0) {
         log.error("unterminated list", token)
       }
 
+      // Saw a ] before seeing for or times (see below), this must have been a literal list
       if (tokens[0].token === "]") {
         tokens.shift()
         return { type: "list", mode: "literal", nodes, token }
       }
 
-      // Finish processing a for list
+      // If we see the `for` keyword, then we're parsing a `for` special form
+      // [ <nodes:node>* for <variable:identifier> in <expression:iterable> ]
       if (tokens[0].token === "for") {
-        log.debug("parseList - found for", token)
-        tokens.shift() // for
+        // Consume the for and parse what the variable we're looping over is
+        tokens.shift()
         let variable = parseIdentifier()
-        log.debug("parseList - found for", token, { variable })
 
+        // Error states: no more input or no 'in' keyword after the variable name
         if (tokens.length === 0) {
           log.error("unterminated for-list", token)
         } else if (tokens[0].token !== "in") {
@@ -346,36 +380,46 @@ export default function parse(tokens) {
         }
         tokens.shift() // in
 
+        // The expression should return any iterable object (we're fuzzy on times)
+        // We only check this at runtime, for now, just parse an expression
         let expression = parseExpression()
-        log.debug("parseList - found for", token, { expression })
 
+        // The for list is done, make sure that we have a closing ]
         if (tokens.length === 0) {
           log.error("unterminated for-list", token)
         } else if (tokens[0].token != "]") {
           log.error("unterminated for-list", token, { expected: "]", got: tokens[0] })
         }
 
+        // Consume the closing ] and return the `list` with mode `for`
         tokens.shift()
         return { type: "list", mode: "for", body: { type: "group", nodes }, variable, expression, token }
       }
 
-      // Finish processing a times list
+      // If we see the `times` keyword, then we're parsing the `times` special form
+      // [ <nodes:node>* times <variable:integer> ]
       if (tokens[0].token === "times") {
+        // Consume the times
         tokens.shift()
-        let body = nodes[0]
+
+        // Parse an expression, this should evaluate to a numeric value but we'll deal with that at runtime
         let expression = parseExpression()
 
+        // That's everything for a times list, so make sure we have a closing ]
         if (tokens.length === 0) {
           log.error("unterminated times-list", token)
         } else if (tokens[0].token != "]") {
           log.error("unterminated times-list", token, { expected: "]", got: tokens[0] })
         }
 
+        // Consume the closing ] and return the `times` `list`
         tokens.shift()
         return { type: "list", mode: "times", body: { type: "group", nodes }, expression, token }
       }
 
-      // Otherwise, build up more nodes or groups
+      // Otherwise we don't know what kind of list it is yet
+      // But for all three cases, we will build up more nodes or groups
+      // The next token ({, define, or anything else) defines what sort of child we're parsing next
       if (tokens[0].token === "{") {
         nodes.push(parseGroup())
       } else if (tokens[0].token === "define") {
@@ -384,6 +428,7 @@ export default function parse(tokens) {
         nodes.push(parseNode())
       }
 
+      // As we do in groups, apply the `.next` parameter
       if (nodes.length >= 2) {
         nodes[nodes.length - 2].next = nodes[nodes.length - 1]
       }
