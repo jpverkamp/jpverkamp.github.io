4aec79dfb4b7e16f3d44270c8ba34e691b8add6e
JP Verkamp <github@jverkamp.com>
2022-07-25 17:16:40 -0400

    Automatic deployment (Remove draft)

--- custom.js
+++ custom.js
@@ -64,3 +64,35 @@ $(function() {
         }
     })
 });
+
+// Ranking buttons
+document.querySelectorAll("a[data-ranked-show]").forEach((el) => {
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+        const list = el.closest(".ranking");
+
+        list.querySelectorAll('a[data-ranked-show').forEach((a) => {
+            if (a === el) {
+                a.classList.add("disabled-link");
+            } else {
+                a.classList.remove("disabled-link");
+            }
+        });
+
+        if (el.dataset["rankedShow"] === "current") {
+            list
+                .querySelectorAll("li[data-future-ranking]")
+                .forEach((li) => {
+                    li.style.display = "none";
+                });
+        } else {
+            list
+                .querySelectorAll("li[data-future-ranking]")
+                .forEach((li) => {
+                    li.style.display = "list-item";
+                });
+        }
+    });
+});
+
+document.querySelectorAll("a[data-ranked-show=\"current\"]").forEach((el) => el.click());
